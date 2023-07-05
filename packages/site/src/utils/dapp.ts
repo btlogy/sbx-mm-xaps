12c9d61ebd7f769b81a27b53fe4435eeb837acc4
Benoit Donneaux <benoit@leastauthority.com>
2023-07-05 14:27:43 +0200

    Fix lint errors

--- packages/site/src/utils/dapp.ts
+++ packages/site/src/utils/dapp.ts
@@ -1,16 +1,16 @@
-/*********************************************/
-/* Access the user's accounts (per EIP-1102) */
-/*********************************************/
-
+/**
+ * Get the user's accounts (per EIP-1102)
+ *
+ * @returns The account id string(s) available in MetaMask.
+ */
 export const getAccounts = async (): Promise<Array<string> | []> => {
-    const accounts = await window.ethereum.request({
+  const accounts = await window.ethereum.request({
     method: 'eth_requestAccounts',
   });
 
-  // Honor our promise and retrun only an Array 
+  // Honor our promise and retrun only an Array
   if (accounts instanceof Array) {
     return accounts;
-  } else {
-    return [];
   }
+  return [];
 };
